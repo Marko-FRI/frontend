418d7f2ce3345b758f31d258dfa3c0e14b04350e
Marko <75992135+Marko-FRI@users.noreply.github.com>
2023-01-13 16:12:52 +0100

    Delete dist/spa directory

--- dist/spa/assets/LogoutPage.931e759c.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./UserStore.1a9949b5.js";import{_ as r,E as o,I as s}from"./index.2a4f541f.js";const u={setup(){return{userStore:t()}},mounted(){this.userStore.token!==null&&(this.userStore.token=null,this.userStore.data=null),this.$router.push("/")}};function n(e,a,c,i,p,l){return o(),s("div")}var d=r(u,[["render",n]]);export{d as default};
--- dist/spa/assets/use-dark.1f412357.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.2a4f541f.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
