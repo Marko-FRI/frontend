0b535690bd09c5cb7ebc934beefee512bbb1e22d
Marko <75992135+Marko-FRI@users.noreply.github.com>
2023-01-13 14:47:46 +0100

    Add files via upload

---
+++ dist/spa/assets/LogoutPage.05699275.js
@@ -0,0 +1,1 @@
+import{u as t}from"./UserStore.0e76a966.js";import{_ as r,E as o,I as s}from"./index.21c77581.js";const u={setup(){return{userStore:t()}},mounted(){this.userStore.token!==null&&(this.userStore.token=null,this.userStore.data=null),this.$router.push("/")}};function n(e,a,c,i,p,l){return o(),s("div")}var d=r(u,[["render",n]]);export{d as default};
---
+++ dist/spa/assets/UserStore.0e76a966.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index.21c77581.js";const r=e("user",{state:()=>({token:null,data:null}),persist:!0});export{r as u};
