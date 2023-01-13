8371618ea2763703b523ccd3cc90bb812460a766
Marko <75992135+Marko-FRI@users.noreply.github.com>
2023-01-13 16:49:34 +0100

    Update CategoryCard.vue

--- src/components/HomePageComponents/CategoryCard.vue
+++ src/components/HomePageComponents/CategoryCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="categories_card col-sm-2 col-6">
     <div class="categories_card__content">
-      <img src="src/components/HomePageComponents/category.PNG">
+      <img src="../HomePageComponents/category.PNG">
       {{ category.name }}
     </div>
   </div>
